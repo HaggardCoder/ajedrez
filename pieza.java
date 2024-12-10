97b5c7edef7d799dc3cf8f638c6202a454cabab4
HaggardCoder <eduardomqesco@gmail.com>
2024-12-10 12:51:46 +0100

    Update and rename pieza to pieza.java

---
+++ pieza.java
@@ -0,0 +1,26 @@
+public class pieza
+	{
+		static int pcolor;
+		static boolean pactivo;
+		public pieza(int color,boolean activo)
+		{
+			String figura=" ";
+			pcolor=color;
+			pactivo=activo;
+		}
+		public static boolean movimiento(int a,int b,int c,int d)
+		{
+			
+			return false;
+			
+		}
+		public int getcolor()
+		{
+			return pcolor;
+		}
+		public boolean getactivo()
+		{
+			return pactivo;
+		}
+		
+	}
