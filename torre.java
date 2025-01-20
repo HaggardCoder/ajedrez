97abdbe6c99ad04e657b7c18c1999f96d79a7eb9
HaggardCoder <eduardomqesco@gmail.com>
2025-01-20 11:43:44 +0100

    Update torre.java

--- torre.java
+++ torre.java
@@ -1,82 +1,101 @@
-public class torre extends pieza
-{
-	public torre(int color,boolean activo)
-	{
-		super(color,activo);
-		if(color==0)
-		{
-			String figura="t";
-		}
-		else
-		{
-			String figura="T";
-		}
+package chess;
+
+public class torre extends pieza {
+
+	public torre(boolean color) {
+		super(color);
+		// TODO Auto-generated constructor stub
 	}
-	public static boolean movimiento(pieza[][] tablero, int a,int b,int c,int d,int turn)
+	public boolean movimiento(int a,int b,int c,int d,tablero tablero)
 	{
-		int e=0;
-		if(tablero[a][b].getcolor()==turn)
+		int e;
+		if(a==c&&b<d)
 		{
-			if(tablero[a][b].getcolor()==tablero[c][d].getcolor())
+			for(e=b+1;e<=d;e++)
 			{
-				return false;					
-			}
-			else
-			{
-				if(a==c && b!=d)
+				if(e==d)
 				{
-					e=b;
-					while(e!=d-1)
-					{
-						if(e<d)
-						{
-							e++;
-						}
-						else
-						{
-							e--;
-						}
-						if(tablero[c][e].getactivo())
-						{
-							return false;
-						}
-					}
-					tablero[c][d]=tablero[a][b];
-					
+					tablero.setpieza(tablero.getpieza(a, b), c, d);
+					tablero.removepieza(a, b);
 					return true;
 				}
-				else if(b==d && a!=c)
+				else if(tablero.getpieza(a, e)!=null)
 				{
-					while(a!=c-1)
-					{
-						if(a<c)
-						{
-							b++;
-						}
-						else
-						{
-							b--;
-						}
-						if(tablero[a][d].getactivo())
-						{
-							return false;
-						}
-					}
-					tablero[c][d]=tablero[a][b];
-					return true;
+					return false;
 				}
 				else
 				{
-					return false;
+					continue;
 				}
 			}
 			
 		}
-		else
+
+		if(a==c&&b>d)
 		{
-			return false;
+			for(e=b-1;e>=d;e--)
+			{
+				if(e==d)
+				{
+					tablero.setpieza(tablero.getpieza(a, b), c, d);
+					tablero.removepieza(a, b);
+					return true;
+				}
+				else if(tablero.getpieza(a, e)!=null)
+				{
+					return false;
+				}
+				else
+				{
+					continue;
+				}
+			}
 		}
-		
+
+		if(a<c&&b==d)
+		{
+			for(e=a+1;e<=c;e++)
+			{
+				if(e==c)
+				{
+					tablero.setpieza(tablero.getpieza(a, b), c, d);
+					tablero.removepieza(a, b);
+					return true;
+				}
+				else if(tablero.getpieza(e, b)!=null)
+				{
+					return false;
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+
+		if(a>c&&b==d)
+		{
+			for(e=a-1;e>=c;e--)
+			{
+				if(e==c)
+				{
+					tablero.setpieza(tablero.getpieza(a, b), c, d);
+					tablero.removepieza(a, b);
+					return true;
+				}
+				else if(tablero.getpieza(e, b)!=null)
+				{
+					return false;
+				}
+				else
+				{
+					continue;
+				}
+				
+			}
+		}
+		return false;
 	}
-	
+
 }
+
