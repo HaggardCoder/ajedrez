6e484a5d3b212ae80134bb0f92acd780ae77de68
HaggardCoder <eduardomqesco@gmail.com>
2025-01-20 11:42:58 +0100

    Update reina.java

--- reina.java
+++ reina.java
@@ -1,149 +1,175 @@
-public class reina extends pieza
-{
-public reina(int color,boolean activo)
-	{
-		super(color,activo);
-		if(color==0)
-		{
-			String figura="q";
-		}
-		else
-		{
-			String figura="Q";
-		}
+package chess;
+
+public class reina extends pieza{
+
+	public reina(boolean color) {
+		super(color);
+		// TODO Auto-generated constructor stub
 	}
-	public static boolean movimiento(pieza[][] tablero, int a,int b,int c,int d,int turn)
+	public boolean movimiento(int a,int b,int c,int d,tablero tablero)
 	{
-		int e=0,x=0;
-		
-		if(tablero[a][b].getcolor()==turn)
+		int e,f;
+		if(a==c&&b<d)
 		{
-			if(tablero[a][b].getcolor()==tablero[c][d].getcolor())
+			for(e=b+1;e<=d;e++)
 			{
-				return false;				
-			}
-			else
-			{
-				if(a==c && b!=d)
+				if(e==d)
 				{
-					e=b;
-					while(e!=d-1)
-					{
-						if(e<d)
-						{
-							e++;
-						}
-						else
-						{
-							e--;
-						}
-						if(tablero[c][e].getactivo())
-						{
-							return false;
-						}
-					}
-					tablero[c][d]=tablero[a][b];
-					
+					tablero.setpieza(tablero.getpieza(a, b), c, d);
+					tablero.removepieza(a, b);
 					return true;
 				}
-				
-				 if(b==d && a!=c)
+				else if(tablero.getpieza(a, e)!=null)
 				{
-					while(a!=c-1)
-					{
-						if(a<c)
-						{
-							b++;
-						}
-						else
-						{
-							b--;
-						}
-						if(tablero[a][d].getactivo())
-						{
-							return false;
-						}
-					}
-					tablero[c][d]=tablero[a][b];
-					return true;
+					return false;
 				}
-				e=a;
-				x=b;
-				while(e>=0 && e<=7&& x>=0&&x<=0)
+				else
 				{
-					e++;
-					x++;
-					if(e==c&&x==b)
-					{
-						return true;
-						
-					}
-					if(tablero[e][x].getactivo())
-					{
-						break;
-					}
+					continue;
 				}
-				e=a;
-				x=b;
-				while(e>=0 && e<=7&& x>=0&&x<=0)
-				{
-					e++;
-					x--;
-					if(e==c&&x==b)
-					{
-						return true;
-						
-					}
-					if(tablero[e][x].getactivo())
-					{
-						break;
-					}
-				}
-				e=a;
-				x=b;
-				while(e>=0 && e<=7&& x>=0&&x<=0)
-				{
-					e--;
-					x++;
-					if(e==c&&x==b)
-					{
-						return true;
-						
-					}
-					if(tablero[e][x].getactivo())
-					{
-						break;
-					}
-				}
-				e=a;
-				x=b;
-				while(e>=0 && e<=7&& x>=0&&x<=0)
-				{
-					e--;
-					x--;
-					if(e==c&&x==b)
-					{
-						return true;
-						
-					}
-					if(tablero[e][x].getactivo())
-					{
-						break;
-					}
-				}
-				return false;
-					
-					
-				}
-				
-							
 			}
-		else
-		{
-			return false;
+			
 		}
-			
+
+		if(a==c&&b>d)
+		{
+			for(e=b-1;e>=d;e--)
+			{
+				if(e==d)
+				{
+					tablero.setpieza(tablero.getpieza(a, b), c, d);
+					tablero.removepieza(a, b);
+					return true;
+				}
+				else if(tablero.getpieza(a, e)!=null)
+				{
+					return false;
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+
+		if(a<c&&b==d)
+		{
+			for(e=a+1;e<=c;e++)
+			{
+				if(e==c)
+				{
+					tablero.setpieza(tablero.getpieza(a, b), c, d);
+					tablero.removepieza(a, b);
+					return true;
+				}
+				else if(tablero.getpieza(e, b)!=null)
+				{
+					return false;
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+
+		if(a>c&&b==d)
+		{
+			for(e=a-1;e>=c;e--)
+			{
+				if(e==c)
+				{
+					tablero.setpieza(tablero.getpieza(a, b), c, d);
+					tablero.removepieza(a, b);
+					return true;
+				}
+				else if(tablero.getpieza(e, b)!=null)
+				{
+					return false;
+				}
+				else
+				{
+					continue;
+				}
+			}
+		}
+		f=d;
+		if(a>c&&b>d)
+		for(e=a+1;e<=7;e++)
+		{
+			f++;
+			if(f>=7)
+				return false;
+			if(c==e&&f==d)
+			{
+				tablero.setpieza(tablero.getpieza(a, b), c, d);
+				tablero.removepieza(a, b);
+				return true;
+			}
+			else if(tablero.getpieza(e, f)!=null)
+			{
+				return false;							
+			}
+		}
+		
+		if(a>c&&b<d)
+		for(e=a+1;e<=7;e++)
+		{
+			f--;
+			if(f<=0)
+				return false;
+			if(c==e&&f==d)
+			{
+				tablero.setpieza(tablero.getpieza(a, b), c, d);
+				tablero.removepieza(a, b);
+				return true;
+			}
+			else if(tablero.getpieza(e, f)!=null)
+			{
+				return false;							
+			}
+		}
+		if(a<c&&b<d)
+		for(e=a+1;e<=0;e--)
+		{
+			f--;
+			if(f<=0)
+				return false;
+			if(c==e&&f==d)
+			{
+				tablero.setpieza(tablero.getpieza(a, b), c, d);
+				tablero.removepieza(a, b);
+				return true;
+			}
+			else if(tablero.getpieza(e, f)!=null)
+			{
+				return false;
+												
+			}
+		}
+		if(a<c&&b>d)
+		for(e=a+1;e<=0;e--)
+		{
+			f++;
+			if(f>=7)
+				break;
+			if(c==e&&f==d)
+			{
+				tablero.setpieza(tablero.getpieza(a, b), c, d);
+				tablero.removepieza(a, b);
+				return true;
+			}
+			else if(tablero.getpieza(e, f)!=null)
+			{
+				return false;
+				
+				
+			}
+		}
+		return false;
 			
 		
 		
-	}}
+	}
+
+}
